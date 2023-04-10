bd236422d428329467df6355832f3f15717ea8c4
EUNSOLY <dmsthf9596@gmail.com>
2023-04-10 22:50:41 +0900

    스타벅스 클론코딩 연습

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-console.log("js");
-
 const searchEl = document.querySelector(".search");
 const searchInputEl = searchEl.querySelector("input");
 
@@ -28,6 +26,10 @@ window.addEventListener(
   _.throttle(function () {
     console.log(window.scrollY);
     if (window.scrollY > 500) {
+      //배지 숨기기
+      // gsap.to(요소, 지속시간, 옵션)
+      // 애니메이션처리해주는 라이브러리에서 지원해주는 메소드
+
       gsap.to(badgeEl, 0.6, {
         opacity: 0,
         display: "none",
@@ -40,5 +42,21 @@ window.addEventListener(
     }
   }, 300)
 );
-
 // _.throttle(함수, 시간)
+
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+console.log(fadeEls);
+fadeEls.forEach(function (item, i) {
+  gsap.to(item, 1, {
+    delay: (i + 1) * 0.7,
+    opacity: 1,
+  });
+});
+
+// 공지사항 슬라이드
+// new Swiper (선택자, 옵션)
+new Swiper(".notice-line .swiper", {
+  direction: "vertical",
+  autoplay: true,
+  loop: true,
+});
